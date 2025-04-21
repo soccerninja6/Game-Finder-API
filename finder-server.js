bef49d6b21b0a15dd446db9930767e8665bd0c21
Cody Meyer <codymeyer@Codys-Mac-Studio.local>
2025-04-21 10:55:27 -0400

    Use Dynamic Port

--- finder-server.js
+++ finder-server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const MIN_PLACE_ID = 10000000;
 const MAX_PLACE_ID = 1700000000;
